ec16a8c2dc78ec6a198137a23609765913486d45
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-20 16:51:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
-
-def countdown
-end
+def countdown(x)
+  x = 10
+  while x > 0 
+  puts "#{x} SECOND(S)!"
+  x -= 1
+  if x == 0 
+  return "HAPPY NEW YEAR!"
+ end
+ end
+ end
+ 
+ def countdown_with_sleep(y)
+  y = 10
+  while y > 0 
+  sleep (1)
+    puts "#{y} SECOND(S)!"
+    
+  y -= 1
+  if y == 0 
+  return "HAPPY NEW YEAR!"
+ end
+ end
+ end
